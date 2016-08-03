8708c7ed946d418254e2208eae3299d273ffc63b
hiren patel <hpdeveloper28@gmail.com>
2016-08-03 14:50:05 +0530

    Removed unnecessary code
    Context removed

--- app/src/main/java/com/materialnavigationdrawer/MainActivity.java
+++ app/src/main/java/com/materialnavigationdrawer/MainActivity.java
@@ -1,11 +1,9 @@
 package com.materialnavigationdrawer;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -13,11 +11,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-
-    private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
